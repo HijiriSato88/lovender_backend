5b1e1d0825d800a31b4c2ea1ae04024a972bb0d9
HijiriSato88 <satohiji0808@outlook.com>
2025-09-22 22:31:16 +0900

    add register and login api

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -10,6 +10,10 @@ func SetupRoutes(e *echo.Echo, userHandler *handler.UserHandler) {
 	// API ルート
 	api := e.Group("/api")
 
-	// ユーザー情報取得
+	// 認証
+	api.POST("/auth/register", userHandler.Register)
+	api.POST("/auth/login", userHandler.Login)
+
+	// API接続テスト用のユーザー情報取得
 	api.GET("/users/:id", userHandler.GetUser)
 }
